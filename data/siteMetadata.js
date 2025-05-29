15fbd5fb3df56df7c3b3b6684d433b54de9b5ec6
Mohammad Aadil Shabier <aadilshabier1@gmail.com>
2025-05-29 13:54:50 +0300

    Let's go!

---
+++ data/siteMetadata.js
@@ -0,0 +1,44 @@
+/** @type {import("pliny/config").PlinyConfig } */
+const siteMetadata = {
+  title: 'Aadil\'s Blog',
+  author: 'Aadil',
+  headerTitle: 'Aadil\'s Blog',
+  description: 'Some cool stuff, maybe',
+  language: 'en-us',
+  theme: 'system', // system, dark or light
+  siteUrl: 'https://aadilshabier.github.io',
+  siteRepo: 'https://github.com/aadilshabier/aadilshabier.github.io',
+  siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
+  socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
+  mastodon: 'https://mastodon.social/@mastodonuser',
+  email: 'address@yoursite.com',
+  github: 'https://github.com',
+  x: 'https://twitter.com/x',
+  // twitter: 'https://twitter.com/Twitter',
+  facebook: 'https://facebook.com',
+  youtube: 'https://youtube.com',
+  linkedin: 'https://www.linkedin.com',
+  threads: 'https://www.threads.net',
+  instagram: 'https://www.instagram.com',
+  medium: 'https://medium.com',
+  bluesky: 'https://bsky.app/',
+  locale: 'en-US',
+  // set to true if you want a navbar fixed to the top
+  stickyNav: false,
+  search: {
+    provider: 'kbar', // kbar or algolia
+    kbarConfig: {
+      searchDocumentsPath: `${process.env.BASE_PATH || ''}/search.json`, // path to load documents to search
+    },
+    // provider: 'algolia',
+    // algoliaConfig: {
+    //   // The application ID provided by Algolia
+    //   appId: 'R2IYF7ETH7',
+    //   // Public API key: it is safe to commit it
+    //   apiKey: '599cec31baffa4868cae4e79f180729b',
+    //   indexName: 'docsearch',
+    // },
+  },
+}
+
+module.exports = siteMetadata
